6b48415ac73a40d8555830cd4864aaa7f1386f94
DabuEdison <202210022@gordoncollege.edu.ph>
2025-02-15 19:49:40 +0800

    updatess

--- act1_app/act1_app/settings.py
+++ act1_app/act1_app/settings.py
@@ -1,3 +1,6 @@
+import os  # Correct import
+from pathlib import Path
+
 """
 Django settings for act1_app project.
 
@@ -114,11 +117,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'act1_app/assets'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
