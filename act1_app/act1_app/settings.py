148a42861f26789170389b91b3636fc7b6f3336f
DabuEdison <202210022@gordoncollege.edu.ph>
2025-02-15 21:36:01 +0800

    changes

--- act1_app/act1_app/settings.py
+++ act1_app/act1_app/settings.py
@@ -120,10 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'act1_app/assets'),
+    BASE_DIR /  'assets',
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
